6296b119b48c6ba1ea70f3a461a2a088d60b4efe
Rex-Prime <rexdotprime@gmail.com>
2025-09-16 15:21:51 +0500

    disabled amdgpu and added hardware-configuration(smh) -Brave +Librewolf

--- modules/opengl.nix
+++ modules/opengl.nix
@@ -4,17 +4,30 @@
 
 services.xserver.videoDrivers = ["amdgpu"];
 
-hardware = {
-  graphics = {
+hardware.graphics = {
   enable = true;
-  
 
   extraPackages = with pkgs; [
+  
+  libva
+  libva-utils
+  
   mesa
+  
+  libva
+  libva-utils
+  
   vulkan-loader
   vulkan-tools
   vulkan-validation-layers
+
+  amdvlk
+  rocmPackages.clr.icd
    ];
-  };
- };
+   extraPackages32 = with pkgs; [
+      # 32-bit support for compatibility
+      driversi686Linux.mesa
+      driversi686Linux.amdvlk
+    ];
+   };
 }

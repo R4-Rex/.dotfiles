6da735054b3100613e494d43c00ddb6a2fd7fdc5
Rex-Prime <rexdotprime@gmail.com>
2026-05-19 17:01:22 +0500

    hyprland: changed bibata cursor from classic to ice

--- modules/home-manager/hyprland/cursors.nix
+++ modules/home-manager/hyprland/cursors.nix
@@ -4,16 +4,16 @@ let
 bibataHyprcursor = pkgs.stdenvNoCC.mkDerivation {
   name = "bibata-hyprcursor";
   src = pkgs.fetchurl {
-    url = "https://github.com/LOSEARDES77/Bibata-Cursor-hyprcursor/releases/download/1.0/hypr_Bibata-Modern-Classic.tar.gz";
-    hash = "sha256-+ZXnbI3bBLcb0nv2YW3eM/tK4dsraNM4UAO9BpSqfXk=";
+    url = "https://github.com/LOSEARDES77/Bibata-Cursor-hyprcursor/releases/download/1.0/hypr_Bibata-Modern-Ice.tar.gz";
+    hash = "sha256-3ttG6Hnr9TPtvIiIbQrsSodu5iZV4Y62xaKvQmkdLPg=";
   };
   
   installPhase = ''
-    mkdir -p $out/share/icons/Bibata-Modern-Classic
-    tar -xzf $src -C $out/share/icons/Bibata-Modern-Classic
+    mkdir -p $out/share/icons/Bibata-Modern-Ice
+    tar -xzf $src -C $out/share/icons/Bibata-Modern-Ice
 
-    cp $out/share/icons/Bibata-Modern-Classic/hyprcursors/left_ptr.hlc \
-    $out/share/icons/Bibata-Modern-Classic/hyprcursors/default.hlc
+    cp $out/share/icons/Bibata-Modern-Ice/hyprcursors/left_ptr.hlc \
+    $out/share/icons/Bibata-Modern-Ice/hyprcursors/default.hlc
 
     '';
   };
@@ -26,7 +26,7 @@ in {
 # Set cursor theme for user sessions
   home.pointerCursor = {
     package = bibataHyprcursor;
-    name = "Bibata-Modern-Classic";
+    name = "Bibata-Modern-Ice";
     size = 28;
     gtk.enable = true;
     x11.enable = true;

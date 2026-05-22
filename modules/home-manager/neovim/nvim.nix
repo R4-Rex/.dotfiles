3150bda9e2f6d161f8de59a82a45c9523db76ec8
Rex-Prime <rexdotprime@gmail.com>
2026-05-22 17:51:57 +0500

    use mkOutOfStoreSymlink for stylua

--- modules/home-manager/neovim/nvim.nix
+++ modules/home-manager/neovim/nvim.nix
@@ -8,5 +8,5 @@
   home.packages = [
     neonix.packages.${pkgs.stdenv.hostPlatform.system}.default
   ];
-  xdg.configFile."stylua/stylua.toml".source = ./stylua.toml;
+  xdg.configFile."stylua/stylua.toml".source = config.lib.file.mkOutOfStoreSymlink ./stylua.toml;
 }

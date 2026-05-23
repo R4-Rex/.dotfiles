7eb829e55f057a97e7b914e1e77d85e01ab25a63
Rex-Prime <rexdotprime@gmail.com>
2026-05-23 17:36:31 +0500

    fix: remove kitty from home-manager

--- modules/home-manager/kitty/kitty.nix
+++ modules/home-manager/kitty/kitty.nix
@@ -1,12 +1,9 @@
 {
   config,
-  pkgs,
   userSettings,
   ...
 }:
 {
-  home.packages = with pkgs; [ kitty ];
-
   xdg.configFile."kitty" = {
     source = config.lib.file.mkOutOfStoreSymlink "${userSettings.dotpath}/modules/home-manager/kitty/.config/";
     recursive = true;

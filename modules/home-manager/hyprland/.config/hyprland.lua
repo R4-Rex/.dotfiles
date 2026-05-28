be2afb983a622c919fd0e503ca8cf6bbc889d83b
Rex-Prime <rexdotprime@gmail.com>
2026-05-28 11:23:31 +0500

    feat: added auto numlock on launch

--- modules/home-manager/hyprland/.config/hyprland.lua
+++ modules/home-manager/hyprland/.config/hyprland.lua
@@ -218,6 +218,8 @@ hl.config({
     kb_options = "",
     kb_rules = "",
 
+    numlock_by_default = true,
+
     follow_mouse = 1,
 
     sensitivity = -0.4, -- -1.0 - 1.0, 0 means no modification.

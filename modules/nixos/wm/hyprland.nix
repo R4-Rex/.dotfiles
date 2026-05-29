10ed9231f2b2761b19b22fa365f29184248c3b6a
Rex-Prime <rexdotprime@gmail.com>
2026-05-29 14:04:33 +0500

    feat: add hyprland withUWSM option, but kept it disabled

--- modules/nixos/wm/hyprland.nix
+++ modules/nixos/wm/hyprland.nix
@@ -2,6 +2,7 @@
 {
   programs.hyprland = {
     enable = true;
+    withUWSM  = false;
     # set the flake package
     package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
     # make sure to also set the portal package, so that they are in sync

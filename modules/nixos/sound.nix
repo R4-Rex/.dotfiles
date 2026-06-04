9c04c7ac794b469ec5c4e2ef11da26f567b77d72
Rex-Prime <rexdotprime@gmail.com>
2026-06-04 21:55:13 +0500

    feat: add cli tools for audio, with bluetooth support & formatted the file
    - tools added: pavucontrol, pamixer, bluez, blues-tools

--- modules/nixos/sound.nix
+++ modules/nixos/sound.nix
@@ -11,49 +11,60 @@
     #jack.enable = true;
     wireplumber = {
       enable = true;
-      /* # alc3220 was the soundcard i was able to detect, which was causing the issue
-      extraConfig = {
-        "alc3220-fix" = {
-          "monitor.alsa.rules" = [
-            {
-              matches = [ { "device.name" = "alsa_card.pci-*-0"; } ];
-              actions = {
-                update-props = {
-                  "api.alsa.auto-mute" = false;
-                  "api.alsa.soft-mixer" = true;
-                  # "api.alsa.disable-mixer-path" = true;
+      /*
+        # alc3220 was the soundcard i was able to detect, which was causing the issue
+        extraConfig = {
+          "alc3220-fix" = {
+            "monitor.alsa.rules" = [
+              {
+                matches = [ { "device.name" = "alsa_card.pci-*-0"; } ];
+                actions = {
+                  update-props = {
+                    "api.alsa.auto-mute" = false;
+                    "api.alsa.soft-mixer" = true;
+                    # "api.alsa.disable-mixer-path" = true;
+                  };
                 };
-              };
-            }
-          ];
+              }
+            ];
+          };
         };
-      };
       */
     };
-/*
-  # the code below wasnt the solution, its just there.. if i need it.
-  # Strong post-start restore (runs after WirePlumber)
-    systemd.user.services."alsa-force-restore" = {
-      description = "Force ALSA state after WirePlumber for ALC3220";
-      after = [ "wireplumber.service" ];
-      wantedBy = [ "pipewire.service" "graphical-session.target" ];
-      serviceConfig = {
-        Type = "oneshot";
-        RemainAfterExit = true;
-        ExecStart = pkgs.writeShellScript "alsa-force-restore" ''
-          sleep 3
-          ${pkgs.alsa-utils}/bin/amixer -c 0 sset 'Auto-Mute Mode' Disabled
-          ${pkgs.alsa-utils}/bin/amixer -c 0 sset Speaker unmute
-          ${pkgs.alsa-utils}/bin/amixer -c 0 sset 'Speaker+LO' unmute
-          ${pkgs.alsa-utils}/bin/amixer -c 0 sset 'Line Out' unmute
-          ${pkgs.alsa-utils}/bin/amixer -c 0 sset Master unmute
-          ${pkgs.alsa-utils}/bin/alsactl store
-        '';
-      };
-    };
-*/
-    };
+    /*
+      # the code below wasnt the solution, its just there.. if i need it.
+      # Strong post-start restore (runs after WirePlumber)
+        systemd.user.services."alsa-force-restore" = {
+          description = "Force ALSA state after WirePlumber for ALC3220";
+          after = [ "wireplumber.service" ];
+          wantedBy = [ "pipewire.service" "graphical-session.target" ];
+          serviceConfig = {
+            Type = "oneshot";
+            RemainAfterExit = true;
+            ExecStart = pkgs.writeShellScript "alsa-force-restore" ''
+              sleep 3
+              ${pkgs.alsa-utils}/bin/amixer -c 0 sset 'Auto-Mute Mode' Disabled
+              ${pkgs.alsa-utils}/bin/amixer -c 0 sset Speaker unmute
+              ${pkgs.alsa-utils}/bin/amixer -c 0 sset 'Speaker+LO' unmute
+              ${pkgs.alsa-utils}/bin/amixer -c 0 sset 'Line Out' unmute
+              ${pkgs.alsa-utils}/bin/amixer -c 0 sset Master unmute
+              ${pkgs.alsa-utils}/bin/alsactl store
+            '';
+          };
+        };
+    */
+  };
+
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+  };
+
   environment.systemPackages = with pkgs; [
     alsa-utils
+    pavucontrol # PulseAudio Volume Control
+    pamixer # Command-line mixer for PulseAudio
+    bluez # Bluetooth support
+    bluez-tools # Bluetooth tools
   ];
 }

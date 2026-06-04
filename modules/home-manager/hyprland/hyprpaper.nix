498107e587b0a30b23278038998a2044e6944c77
Rex-Prime <rexdotprime@gmail.com>
2026-06-04 17:11:30 +0500

    moved hyprpaper to a declarative config

--- modules/home-manager/hyprland/hyprpaper.nix
+++ modules/home-manager/hyprland/hyprpaper.nix
@@ -7,9 +7,27 @@
 }:
 
 {
-  home.packages = with pkgs; [
-    hyprpaper
-  ];
+
+  services.hyprpaper = {
+    enable = true;
+    settings = {
+      ipc = "on";
+      splash = false;
+      splash_offset = 2;
+
+      preload = [
+        "~/wallpapers/batman-logo.jpg"
+      ];
+
+      wallpaper = [
+        {
+          monitor = "VGA-1";
+          path = "~/wallpapers/batman-logo.jpg";
+          fit_mode = "cover";
+        }
+      ];
+    };
+  };
 
   home.file."wallpapers" = {
     source = config.lib.file.mkOutOfStoreSymlink "${userSettings.dotpath}/wallpapers/";

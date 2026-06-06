12dba1a3d4d046d3dfee045ca126ad98ce2f4eda
Rex-Prime <rexdotprime@gmail.com>
2026-06-07 00:38:39 +0500

    formatted librewolf.nix

--- modules/home-manager/browsers/librewolf/librewolf.nix
+++ modules/home-manager/browsers/librewolf/librewolf.nix
@@ -1,35 +1,36 @@
- {config,  pkgs, ...}: {
- 
- imports = [
-    
+{ config, pkgs, ... }:
+{
+
+  imports = [
+
     ./toolbar.nix
- 
- ];
+
+  ];
 
   programs.librewolf = {
     enable = true;
     settings = {
-     "webgl.disabled" = false; # Enables WebGL, which might be needed for some websites and applications :cite[1]:cite[8].
-    
-     # Sync
-     "identity.fxaccounts.enabled" = true;
+      "webgl.disabled" = false; # Enables WebGL, which might be needed for some websites and applications :cite[1]:cite[8].
 
-     # Vertical Tabs
+      # Sync
+      "identity.fxaccounts.enabled" = true;
+
+      # Vertical Tabs
       "sidebar.verticalTabs" = true; # Enable vertical tabs
-      "sidebar.revamp" = true;       # New sidebar design
-      
-     # Url & Search Bar
-     "browser.urlbar.suggest.bookmark" = false; # Don't suggest bookmarks
-       "browser.urlbar.suggest.openpage" = false; # Don't suggest open pages
-       "browser.urlbar.suggest.topsites" = false; # Don't suggest top sites
-   
-     # Mouse Scroll
+      "sidebar.revamp" = true; # New sidebar design
+
+      # Url & Search Bar
+      "browser.urlbar.suggest.bookmark" = false; # Don't suggest bookmarks
+      "browser.urlbar.suggest.openpage" = false; # Don't suggest open pages
+      "browser.urlbar.suggest.topsites" = false; # Don't suggest top sites
+
+      # Mouse Scroll
       "general.autoScroll" = true; # Middle click auto-scroll
-       "toolkit.tabbox.switchByScrolling" = true; # Switch tabs by scrolling
-     
-     # Stylesheet
-    "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
-    
+      "toolkit.tabbox.switchByScrolling" = true; # Switch tabs by scrolling
+
+      # Stylesheet
+      "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+
     };
   };
 }

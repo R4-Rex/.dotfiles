132a649ccfd6e06b4f01d653bec0afa6c1899251
Rex-Prime <rexdotprime@gmail.com>
2026-06-12 23:39:40 +0500

    refactor(librewolf): went back to basic librewolf config, with working profile

--- modules/home-manager/browsers/librewolf/librewolf.nix
+++ modules/home-manager/browsers/librewolf/librewolf.nix
@@ -3,34 +3,37 @@
 
   imports = [
 
-    ./toolbar.nix
-
   ];
 
   programs.librewolf = {
     enable = true;
-    settings = {
-      "webgl.disabled" = false; # Enables WebGL, which might be needed for some websites and applications :cite[1]:cite[8].
+    configPath = ".librewolf/";
+    profiles = {
+      "rex" = {
+        settings = {
+          "webgl.disabled" = false; # Enables WebGL, which might be needed for some websites and applications :cite[1]:cite[8].
 
-      # Sync
-      "identity.fxaccounts.enabled" = true;
+          # Sync
+          "identity.fxaccounts.enabled" = true;
 
-      # Vertical Tabs
-      "sidebar.verticalTabs" = true; # Enable vertical tabs
-      "sidebar.revamp" = true; # New sidebar design
+          # Vertical Tabs
+          "sidebar.verticalTabs" = true; # Enable vertical tabs
+          "sidebar.revamp" = true; # New sidebar design
 
-      # Url & Search Bar
-      "browser.urlbar.suggest.bookmark" = false; # Don't suggest bookmarks
-      "browser.urlbar.suggest.openpage" = false; # Don't suggest open pages
-      "browser.urlbar.suggest.topsites" = false; # Don't suggest top sites
+          # Url & Search Bar
+          "browser.urlbar.suggest.bookmark" = false; # Don't suggest bookmarks
+          "browser.urlbar.suggest.openpage" = false; # Don't suggest open pages
+          "browser.urlbar.suggest.topsites" = false; # Don't suggest top sites
 
-      # Mouse Scroll
-      "general.autoScroll" = true; # Middle click auto-scroll
-      "toolkit.tabbox.switchByScrolling" = true; # Switch tabs by scrolling
+          # Mouse Scroll
+          "general.autoScroll" = true; # Middle click auto-scroll
+          "toolkit.tabbox.switchByScrolling" = true; # Switch tabs by scrolling
 
-      # Stylesheet
-      "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+          # Stylesheet
+          "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
 
+        };
+      };
     };
   };
 }
